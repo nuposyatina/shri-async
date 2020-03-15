923de43eba0c5e222fd503d02dddaad05c39fae4
Anna Tsukanova <nuposyatina@gmail.com>
2020-03-15 14:43:53 +0300

    Исправила ошибки

--- quadratic.js
+++ quadratic.js
@@ -1,15 +1,18 @@
 (() => {
+
+
   const promisify = (fn) => (...args) => {
     return new Promise((resolve) => {
       fn(...args, resolve)
     });
   };
   
-  const sqrt = promisify(window.sqrt);
-  const add = promisify(window.add);
-  const subtract = promisify(window.subtract);
-  const multiply = promisify(window.multiply);
-  const divide = promisify(window.divide);
+  const sqrt = promisify(window.Homework.sqrt);
+  const add = promisify(window.Homework.add);
+  const subtract = promisify(window.Homework.subtract);
+  const multiply = promisify(window.Homework.multiply);
+  const divide = promisify(window.Homework.divide);
+  const less = promisify(window.Homework.less);
   
   const getDiscriminant = (a, b, c) => {
     let sqrB;
@@ -23,23 +26,33 @@
   }
   
   const quadratic = (a, b, c, cb) => {
+    let discriminant;
     let discriminantSqrt;
     let minusB;
     let denom;
     let first;
     let second;
     getDiscriminant(a, b, c).
-    then((discriminant) => sqrt(discriminant)).
-    then((discriminantSqrt) => {
-      discriminantSqrt = discriminantSqrt;
+    then((resultDiscriminant) => {
+      discriminant = resultDiscriminant;
+      return less(discriminant, 0);
+    }).
+    then((isNegative) => {
+      if (isNegative) {
+        return new Promise((_, reject) => reject());
+      }
+      return sqrt(discriminant);
+    }).
+    then((sqrtFromDiscriminant) => {
+      discriminantSqrt = sqrtFromDiscriminant;
       return subtract(0, b);
     }).
-    then((minusB) => {
-      minusB = minusB;
+    then((resultMinusB) => {
+      minusB = resultMinusB;
       return multiply(2, a);
     }).
-    then((denom) => {
-      denom = denom;
+    then((resultDenom) => {
+      denom = resultDenom;
       return add(minusB, discriminantSqrt);
     }).
     then((numer) => divide(numer, denom)).
@@ -51,6 +64,9 @@
     then((result) => {
       second = result;
       return cb(first, second)
+    })
+    .catch(() => {
+      console.log('У квадратного уравнения нет корней')
     });
   };
 
